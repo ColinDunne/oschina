9e45d1279fe5ae9867a0a26ab1dd80066b803d9f
QianChen <xgdjqc@126.com>
2015-07-17 17:31:29 +0800

    add podfile

--- oschina/oschina/MainViewControllers/OSCObjsViewController.h
+++ oschina/oschina/MainViewControllers/OSCObjsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  OSCObjsViewController.h
 //  oschina
 //
 //  Created by 钱辰 on 15/7/17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface OSCObjsViewController : UITableViewController
 
 @end
-
